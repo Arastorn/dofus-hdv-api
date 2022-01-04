28e1f67771bb0ca7eed11f9b88a038222b2075af
Arastorn <pro.abourgeois@gmail.com>
2022-01-04 14:51:37 +0100

    command create crush and aggregate

---
+++ src/Dofus.Tools.Core/Aggregates/CrushesAggregate/Crush.cs
@@ -0,0 +1,33 @@
+﻿using Dofus.Tools.Core.Interfaces;
+using NodaTime;
+
+namespace Dofus.Tools.Core.Aggregates.CrushesAggregate;
+
+public class Crush : IAggregateRoot
+{
+    private Crush()
+    {
+    }
+
+    private Crush(Guid id, long dofusId, short serverId, long value, Instant createdAt, string createdBy)
+    {
+        Id = id;
+        DofusId = dofusId;
+        Value = value;
+        CreatedAt = createdAt;
+        CreatedBy = createdBy;
+        ServerId = serverId;
+    }
+
+    public Guid Id { get; }
+    public long DofusId { get; private set; }
+    public short ServerId { get; private set; }
+    public long Value { get; private set; }
+    public Instant CreatedAt { get; private set; }
+    public string CreatedBy { get; private set; } = default!;
+
+    public static Crush Create(long dofusId, short serverId, long value, Instant createdAt, string createdBy)
+    {
+        return new Crush(Guid.NewGuid(), dofusId, serverId, value, createdAt, createdBy);
+    }
+}
---
+++ src/Dofus.Tools.Core/Features/Commands/CreateCrush/CreatePriceCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Dofus.Tools.Core.Aggregates.CrushesAggregate;
+using Dofus.Tools.Core.Interfaces;
+using MediatR;
+using NodaTime;
+
+namespace Dofus.Tools.Core.Features.Commands.CreateCrush;
+
+public class CreateCrushCommandHandler : IRequestHandler<CreateCrushCommand>
+{
+    private readonly IClock clock;
+    private readonly CrushRepository crushRepository;
+    
+    public CreateCrushCommandHandler(IClock clock, CrushRepository crushRepository)
+    {
+        this.clock = clock;
+        this.crushRepository = crushRepository;
+    }
+
+    public async Task<Unit> Handle(CreateCrushCommand request, CancellationToken cancellationToken)
+    {
+        await crushRepository.Create(Crush.Create(
+                request.DofusId,
+                request.ServerId,
+                request.Value,
+                clock.GetCurrentInstant(),
+                request.CreatedBy),
+            cancellationToken);
+
+        return Unit.Value;
+    }
+}
